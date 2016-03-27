62430a61d5b545a07789c7ca0f7a0ed8ffdbd99f
Hilal Jaffan <hjaffan90@gmail.com>
2016-03-27 12:00:59 -0700

    update templates

--- website/views.py
+++ website/views.py
@@ -3,9 +3,8 @@ from .models import about, services, update, product
 # Create your views here.
 def home(request):
 	context = dict()
-	context['news'] = about.objects.getall()
-	context['products'] = product.objects.getall()
-	context['about'] = product.objects.getall()
-	context['services'] = product.objects.getall()
-
+	context['news'] = update.objects.all()
+	context['products'] = product.objects.all()
+	context['about'] = about.objects.all()
+	context['services'] = product.objects.all()
 	return render(request, template_name="main.html", context=context)
